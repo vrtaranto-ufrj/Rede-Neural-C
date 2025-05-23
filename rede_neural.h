8600f5e0893cae20da8ba73a2ba1501f4300a11b
Vitor <vrtaranto@poli.ufrj.br>
2025-05-23 15:02:49 -0300

    Tentando

--- rede_neural.h
+++ rede_neural.h
@@ -7,23 +7,25 @@ typedef struct NeuronioStruct Neuronio;
 
 
 struct NeuronioStruct {
-    
+    float (*funcao_ativacao)(float);
+
 };
 
 struct RedeNeuralStruct {
     size_t num_entradas;
     size_t num_saidas;
-    size_t num_camadas;
-    size_t *num_neuronios_por_camada;
+
+    size_t num_neuronios;
+
+    size_t *camada_neuronios;
+    float (**funcoes_ativacao_neuronios)(float);
+    Matriz pesos_neuronio;
 
 };
 
 
 void cria_rede_neural(
-    size_t num_entradas,
-    size_t num_saidas,
-    size_t num_camadas,
-    size_t *num_neuronios_por_camada
+    size_t *camada_neuronios
 );
 
 void fit_rede_neural(RedeNeural *rede_neural, Matriz *entrada, Matriz *saida);
